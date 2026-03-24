b58fb482548cab0392ebb0be9f1f42c7adbe9246
Brendan Hawk <brendanhawk03@aol.com>
2026-03-24 08:22:41 -0400

    Changed the way data was fetched from JSON file to use fetch() instead
    I changed to getting the data from JSON file to use the fetch method instead of importing the JSON file data directly.

--- src/display.jsx
+++ src/display.jsx
@@ -1,5 +1,7 @@
 import "./display.css";
-import workoutData from "/workoutData.json";
+//import workoutData from "/workoutData.json";
+
+import { useState, useEffect } from "react";
 
 //I will need to pass data from user down through props
 //I should probably generated the cards more dynamically
@@ -7,17 +9,35 @@ import workoutData from "/workoutData.json";
 //h3 on first card is only targeting the group of "chest" but not the individual exercise
 
 export default function Display() {
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		fetch("./workoutData.json")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then((data) => setData(data))
+			.catch((error) => console.log("Error fetching data", error));
+	}, []);
+
+	if (!data) {
+		return <p>Loading...</p>;
+	}
+
 	return (
 		<>
 			<h2>Here are three exercises for this muscle group</h2>
 			<div className="card-container">
 				<div className="card">
-					<h3>{workoutData.muscle_groups[0].exercises[0].name}</h3>
+					<h3>{data.muscle_groups[0].exercises[0].name}</h3>
 					<br />
-					<p>{workoutData.muscle_groups[0].exercises[0].description}</p>
+					<p>{data.muscle_groups[0].exercises[0].description}</p>
 					<br />
 					<p>
-						Do: {workoutData.muscle_groups[0].exercises[0].sets} Sets with {workoutData.muscle_groups[0].exercises[0].reps} reps
+						Do: {data.muscle_groups[0].exercises[0].sets} Sets with {data.muscle_groups[0].exercises[0].reps} reps
 					</p>
 				</div>
 				<br />

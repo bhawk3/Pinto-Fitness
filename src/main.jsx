713021ad52e37a1490c59e2b7cdb64a2571408c9
Brendan Hawk <brendanhawk03@aol.com>
2026-03-27 07:58:28 -0400

    Data is flowinggggg

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import DisplayData from "./displayData.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<DisplayData />
 	</StrictMode>,
 );

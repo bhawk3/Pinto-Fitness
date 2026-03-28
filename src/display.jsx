8f3f7a78adaa0dde375ae417835ce1a02295b77d
Brendan Hawk <brendanhawk03@aol.com>
2026-03-28 19:35:27 -0400

    This should be close to dynamically adding cards

--- src/display.jsx
+++ src/display.jsx
@@ -1,4 +1,5 @@
 import "./display.css";
+import { useState, useEffect } from "react";
 
 //import { useState, useEffect } from "react";
 
@@ -8,42 +9,44 @@ import "./display.css";
 //h3 on first card is only targeting the group of "chest" but not the individual exercise
 
 export default function Display(props) {
-	console.log("Display props.filter:", props.filter);
-	console.log("Display props.data:", props.data);
+	//console.log("Display props.filter:", props.filter);
+	//console.log("Display props.data:", props.data);
 	const selectedGroup = props.data.muscle_groups.find((g) => g.group.toLowerCase() === props.filter.toLowerCase()) || props.data.muscle_groups[0];
-	console.log("Selected group:", selectedGroup);
-	const exercises = selectedGroup.exercises[0] || [];
+	//	console.log("Selected group:", selectedGroup);
+	const exercises = selectedGroup.exercises || [];
+
+	//Somehow map over the exercises returned
+
+	//prob need to use usememo
+
+	const [randomLift, setRandomLift] = useState([]);
+
+	useEffect(() => {
+		if (exercises.length > 0) {
+			const shuffle = [...exercises].sort(() => Math.random() - 0.5);
+			const selected = shuffle.slice(0, 3);
+
+			setRandomLift(selected);
+		}
+	}, [props.filter, props.data]);
 
 	return (
 		<>
 			<h2>Here are three exercises for this muscle group</h2>
+
 			<div className="card-container">
-				<div className="card">
-					<h3>{exercises.name}</h3>
-					<br />
-					<p>{exercises.description}</p>
-					<br />
-					<p>
-						Do: {exercises.sets} Sets with {exercises.reps} reps
-					</p>
-				</div>
+				{randomLift.map((workout) => (
+					<div className="card" key={workout.id}>
+						<h3>{workout.name}</h3>
+						<br />
+						<p>{workout.description}</p>
+						<br />
+						<p>
+							Do: {workout.sets} Sets with {workout.reps} reps
+						</p>
+					</div>
+				))}
 				<br />
-				<div className="card">
-					<h3>NAME OF EXERCISE</h3>
-					<br />
-					<p>Quick description/instructions</p>
-					<br />
-					<p>Do: xyz Sets with xyz reps</p>
-				</div>
-				<br />
-				<div className="card">
-					<br />
-					<h3>NAME OF EXERCISE</h3>
-					<br />
-					<p>Quick description/instructions</p>
-					<br />
-					<p>Do: xyz Sets with xyz reps</p>
-				</div>
 			</div>
 		</>
 	);
